ebe9960f3b114ba72a0620d664b0a53244b58766
Dustm00n <gerardoserranosoto@gmail.com>
2022-02-09 17:38:05 +0000

    Wakala

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import PaginaOne from "./pagina1.jsx";
+import Gato from "./pagina1.jsx";
 
-//create your first component
-const Principal = () => {
+export function Home() {
+	const [nombre1, setNombre1] = useState("");
+	const [nombre2, setNombre2] = useState("");
+
+	function tomarNombres() {
+		let nombre1 = document.querySelector("#nombre1").value;
+		let nombre2 = document.querySelector("#nombre2").value;
+		setNombre1(nombre1);
+		setNombre2(nombre2);
+		console.log("Tus nombres son:" + nombre1 + " - " + nombre2);
+	}
+
+	const [desaparecer, setDesaparecer] = useState("d-block");
+	const desaparecerLogin = () => {
+		setDesaparecer("d-none");
+	};
+
+	const [aparecerTic, setAparecerTic] = useState("d-none");
+	const aparecerTicTack = () => {
+		setAparecerTic("d-block");
+	};
+
+	const [turno, setTurno] = useState("");
+	const asignarTurno = (XO) => {
+		setTurno(XO);
+	};
+	const turno2 = [...turno];
+
+	const [tablero, setTablero] = useState([
+		["ㅤ", "ㅤ", "ㅤ"],
+		["ㅤ", "ㅤ", "ㅤ"],
+		["ㅤ", "ㅤ", "ㅤ"],
+	]);
+
+	const tablero2 = [...tablero];
+
+	const alternarTurno = () => {
+		if (turno === "X") {
+			setTurno("O");
+		} else if (turno === "O") {
+			setTurno("X");
+		}
+	};
+
 	return (
-		<>
-			<h1>Hola Mundo</h1>
-			<PaginaOne />
-		</>
-	);
-};
+		<div className="container">
+			<div className="container text-white mt-1 ">
+				<div className="row justify-content-center text-center">
+					<div className="col-8 contenedorCentro mt-2 mb-5 p-2">
+						<h1>TicTacToe in React.js</h1>
+						<h2>Es turno de {turno}</h2>
+						<div className="row justify-content-center">
+							<div
+								className={
+									"col-10 contenedorChico p-3 " + desaparecer
+								}>
+								<p className="h3">¿Como se llaman?</p>
 
-export default Principal;
+								<div className="d-flex justify-content-center">
+									<input
+										placeholder="Jugador 1"
+										type="text"
+										className="col-4 mr-2 form-control"
+										id="nombre1"></input>
+									<input
+										type="text"
+										placeholder="Jugador 2"
+										className="col-4 ml-2 form-control"
+										id="nombre2"></input>
+								</div>
+
+								<p className="my-3 h4">¿Quien empieza?</p>
+								<button
+									className="btnTicTac ml-3 mr-2 mb-3 text-warning"
+									id="btnLogin1"
+									onClick={() => {
+										{
+											asignarTurno("X");
+											desaparecerLogin();
+											aparecerTicTack();
+											tomarNombres();
+										}
+									}}>
+									X
+								</button>
+								<button
+									className="btnTicTac m my-3 text-primary"
+									id="btnLogin2"
+									onClick={() => {
+										{
+											asignarTurno("O");
+											desaparecerLogin();
+											aparecerTicTack();
+											tomarNombres();
+										}
+									}}>
+									O
+								</button>
+							</div>
+							<Gato
+								setDesaparecer={setDesaparecer}
+								nombre1={nombre1}
+								nombre2={nombre2}
+								setTurno={setTurno}
+								asignarTurno={asignarTurno}
+								turno2={turno2}
+								tablero2={tablero2}
+								tablero={tablero}
+								setTablero={setTablero}
+								alternarTurno={alternarTurno}
+								turnoActual={turno}
+								aparecerTic={aparecerTic}
+								setAparecerTic={setAparecerTic}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
--- src/js/component/pagina1.jsx
+++ src/js/component/pagina1.jsx
@@ -1,26 +1,262 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+const Gato = (props) => {
+	let cuadronum;
+	let [aparecer, setAparecer] = useState("d-none");
+	let [ganador, setGanador] = useState("");
 
-const PaginaOne = () => {
+	useEffect(() => {
+		let aux = props.tablero2;
+		if (
+			(aux[0][0] == "X" && aux[0][1] == "X" && aux[0][2] == "X") ||
+			(aux[1][0] == "X" && aux[1][1] == "X" && aux[1][2] == "X") ||
+			(aux[2][0] == "X" && aux[2][1] == "X" && aux[2][2] == "X") ||
+			(aux[0][0] == "X" && aux[1][1] == "X" && aux[2][2] == "X") ||
+			(aux[0][2] == "X" && aux[1][1] == "X" && aux[2][0] == "X") ||
+			(aux[0][0] == "X" && aux[1][0] == "X" && aux[2][0] == "X") ||
+			(aux[0][1] == "X" && aux[1][1] == "X" && aux[2][1] == "X") ||
+			(aux[0][2] == "X" && aux[1][2] == "X" && aux[2][2] == "X")
+		) {
+			setAparecer("d-block");
+			console.log("GANADOR");
+			setGanador("X");
+		} else if (
+			(aux[0][0] == "O" && aux[0][1] == "O" && aux[0][2] == "O") ||
+			(aux[1][0] == "O" && aux[1][1] == "O" && aux[1][2] == "O") ||
+			(aux[2][0] == "O" && aux[2][1] == "O" && aux[2][2] == "O") ||
+			(aux[0][0] == "O" && aux[1][1] == "O" && aux[2][2] == "O") ||
+			(aux[0][2] == "O" && aux[1][1] == "O" && aux[2][0] == "O") ||
+			(aux[0][0] == "O" && aux[1][0] == "O" && aux[2][0] == "O") ||
+			(aux[0][1] == "O" && aux[1][1] == "O" && aux[2][1] == "O") ||
+			(aux[0][2] == "O" && aux[1][2] == "O" && aux[2][2] == "O")
+		) {
+			setAparecer("d-block");
+			console.log("GANADOR");
+			setGanador("O");
+		} else {
+			console.log("Aun no hay ganador");
+		}
+	});
+	const cambiarTurno = () => {
+		let aux = props.tablero2;
+		if (aux[0][0] === "X" || aux[0][0] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[0][0] = props.turnoActual;
+
+			props.setTablero(aux);
+
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno1 = () => {
+		let aux = props.tablero2;
+		if (aux[0][1] === "X" || aux[0][1] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[0][1] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno2 = () => {
+		let aux = props.tablero2;
+		if (aux[0][2] === "X" || aux[0][2] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[0][2] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno3 = () => {
+		let aux = props.tablero2;
+		if (aux[1][0] === "X" || aux[1][0] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[1][0] = props.turnoActual;
+
+			props.setTablero(aux);
+
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno4 = () => {
+		let aux = props.tablero2;
+		if (aux[1][1] === "X" || aux[1][1] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[1][1] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno5 = () => {
+		let aux = props.tablero2;
+		if (aux[1][2] === "X" || aux[1][2] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[1][2] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno6 = () => {
+		let aux = props.tablero2;
+
+		if (aux[2][0] === "X" || aux[2][0] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[2][0] = props.turnoActual;
+
+			props.setTablero(aux);
+
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno7 = () => {
+		let aux = props.tablero2;
+		if (aux[2][1] === "X" || aux[2][1] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[2][1] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const cambiarTurno8 = () => {
+		let aux = props.tablero2;
+		if (aux[2][2] === "X" || aux[2][2] === "O") {
+			console.log("No puedes poner un valor aqui");
+		} else {
+			aux[2][2] = props.turnoActual;
+			props.setTablero(aux);
+			props.alternarTurno(...props.turno2);
+		}
+	};
+	const volverAJugar = () => {
+		let aux = [
+			["ㅤ", "ㅤ", "ㅤ"],
+			["ㅤ", "ㅤ", "ㅤ"],
+			["ㅤ", "ㅤ", "ㅤ"],
+		];
+		setAparecer("d-none");
+
+		props.setTablero(aux);
+	};
+	const irAlLogin = () => {
+		props.setDesaparecer("d-block");
+		props.setAparecerTic("d-none");
+		setAparecer("d-none");
+		volverAJugar();
+	};
+
 	return (
-		<div className="Container">
-			<span>Escoge tu Figurita</span>
-			<div className="Cajita-Input">
-				<input
-					type="text"
-					className=""
-					placeholder="Jugador 1 Username"
-				/>
-				<input
-					type="text"
-					className=""
-					placeholder="Jugador 2 Username"
-				/>
+		<div className={"col-10 contenedorChico p-3 " + props.aparecerTic}>
+			<div className={aparecer}>
+				<h1 className="h1 text-center">
+					¡ {ganador == "X" ? props.nombre1 : props.nombre2} ES
+					GANAD@R! {ganador}
+				</h1>
+				<button
+					className="btn btn-danger my-2 mx-2"
+					onClick={volverAJugar}>
+					Quiero volver a jugar!
+				</button>
+				<button
+					className="btn btn-primary my-2 mx-2"
+					onClick={irAlLogin}>
+					Ir al login!
+				</button>
 			</div>
-			<div className="GrupoBotones">
-				<button className="Boton-O">O</button>
-				<button className="Boton-X">X</button>
+			<div className="row justify-content-center">
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-top-0 border-left-0"
+						onClick={cambiarTurno}>
+						{props.tablero[0][0]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-top-0"
+						onClick={cambiarTurno1}>
+						{props.tablero[0][1]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-top-0 border-right-0"
+						onClick={cambiarTurno2}>
+						{props.tablero[0][2]}
+					</button>
+				</div>
 			</div>
+			<div className="row justify-content-center">
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-left-0"
+						onClick={cambiarTurno3}>
+						{props.tablero[1][0]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button className="btnTicTac w-100" onClick={cambiarTurno4}>
+						{props.tablero[1][1]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-right-0"
+						onClick={cambiarTurno5}>
+						{props.tablero[1][2]}
+					</button>
+				</div>
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-left-0 border-bottom-0"
+						onClick={cambiarTurno6}>
+						{props.tablero[2][0]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-bottom-0"
+						onClick={cambiarTurno7}>
+						{props.tablero[2][1]}
+					</button>
+				</div>
+				<div className="col-3 m-0 p-0">
+					<button
+						className="btnTicTac w-100 border-bottom-0 border-right-0"
+						onClick={cambiarTurno8}>
+						{props.tablero[2][2]}
+					</button>
+				</div>
+			</div>
+			<button className="btn btn-warning mt-5" onClick={volverAJugar}>
+				Resetear
+			</button>
 		</div>
 	);
 };
-export default PaginaOne;
+
+Gato.propTypes = {
+	aparecerTic: PropTypes.string,
+	setDesaparecer: PropTypes.string,
+	turnoActual: PropTypes.string,
+	turno2: PropTypes.string,
+	nombre1: PropTypes.string,
+	nombre2: PropTypes.string,
+	tablero: PropTypes.array,
+	setTablero: PropTypes.func,
+	alternarTurno: PropTypes.func,
+	setTurno: PropTypes.func,
+	setAparecerTic: PropTypes.func,
+	asignarTurno: PropTypes.func,
+	tablero2: PropTypes.array,
+};
+
+export default Gato;
